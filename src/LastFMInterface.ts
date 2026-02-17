7eac037785d38f1b61215d65ba3aac7706896394
LeBonsBay <armin.keric0001@gmail.com>
2026-02-17 18:02:52 +0100

    feat: add of `LastFMInterface` interface

---
+++ src/LastFMInterface.ts
@@ -0,0 +1,23 @@
+interface LastFMInterface {
+    artist: {
+        "#text": string
+    },
+    image: {
+        "size":string,
+        "#text":string,
+    }[],
+    album:{
+        "#text":string
+    },
+    name: string,
+    //question mark makes the data optional (e.g. not listening to something on spotify right now)
+    "@attr"?: {
+        "nowplaying": string | boolean,
+    }
+}
+
+interface LastFMTracks{
+    recenttrack:{
+        track: LastFMInterface[];
+    };
+}
---
+++ src/render.ts
@@ -0,0 +1,5 @@
+import * as lastfmts from "./last.fm.ts"
+import {data} from "./last.fm.ts";
+
+//@ToDo
+//user svg for generating img &
